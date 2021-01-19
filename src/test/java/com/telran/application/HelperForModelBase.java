3b629ff2923c3ec07153c01931accdfa11bb3320
TayaHatum <21taya21@gmail.com>
2021-01-19 16:28:16 +0200

    added bases for project

---
+++ src/test/java/com/telran/application/HelperForModelBase.java
@@ -0,0 +1,9 @@
+package com.telran.application;
+
+import org.openqa.selenium.WebDriver;
+
+public class HelperForModelBase extends HelperBase {
+    public HelperForModelBase(WebDriver wd) {
+        super(wd);
+    }
+}
